e82f23ed5f23934c407a76a6bee1c11049058097
김형익 <kiomnd2@naver.com>
2022-04-21 17:44:44 +0900

    exception 수정

--- src/exception/PdfExportException.java
+++ src/exception/PdfExportException.java
@@ -4,6 +4,10 @@ public class PdfExportException extends RuntimeException{
 
 
     public PdfExportException(Exception e) {
-        super("PDF 추출중 오류가 발생했습니다 : " + e.getMessage());
+        this(e.getMessage());
+    }
+
+    public PdfExportException(String s) {
+        super("PDF 추출중 오류가 발생했습니다 : " + s);
     }
 }
